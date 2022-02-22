4f36e6ba5aa506514544041b23d892a5d52b5c1a
Allana Karla <allanakarla1702@gmail.com>
2022-02-22 20:56:17 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,5 +1,32 @@
-describe('', () => {
-  it('', () => {
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { FavoritePokemons } from '../components';
+import App from '../App';
+import renderWithRouter from '../components/RenderWithRouter';
 
+const { screen } = require('@testing-library/react');
+
+describe('Testa o componente <FavoritePokemons.js />', () => {
+  it('Testa se a mensagem No "favorite pokemon found" aparece na tela', () => {
+    // acessar
+    renderWithRouter(<FavoritePokemons />);
+    const favoritePokemonsText = screen.getByText(/No favorite pokemon found/i);
+    // testar
+    expect(favoritePokemonsText).toBeInTheDocument();
+  });
+  it('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    // acessar
+    renderWithRouter(<App />);
+    const linkMoreDetails = screen.getByRole('link', { name: /More details/i });
+    userEvent.click(linkMoreDetails);
+
+    const favoriteCheckbox = screen.getByLabelText(/Pokémon favoritado?/i);
+    userEvent.click(favoriteCheckbox);
+
+    const linkFavoritePokemons = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(linkFavoritePokemons);
+
+    const linkMoreDetailsFav = screen.getByRole('link', { name: /More details/i });
+    expect(linkMoreDetailsFav).toBeInTheDocument();
   });
 });
