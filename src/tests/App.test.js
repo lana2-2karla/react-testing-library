519846acd8801281b5908ba5396d1f320642cc04
Allana Karla <allanakarla1702@gmail.com>
2022-02-21 21:42:40 -0300

    Requisito 1 - dois testes falhando

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,62 @@
-test('', () => {});
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../components/RenderWithRouter';
+
+const { screen } = require('@testing-library/react');
+
+describe('Testa o componente <App.js />', () => {
+  it('Testa se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    // acessar
+    renderWithRouter(<App />);
+    const homeEl = screen.getByRole('link', { name: /Home/i });
+    const aboutEl = screen.getByRole('link', { name: /About/i });
+    const favoritePokemonEl = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    // testar
+    expect(homeEl && aboutEl && favoritePokemonEl).toBeInTheDocument();
+  });
+
+  it('Testa se o app é redirecionado para a URL / ao clicar no link Home', () => {
+    // acessar
+    const { history } = renderWithRouter(<App />);
+    const { pathname } = history.location;
+    const homeEl = screen.getByRole('link', { name: /Home/i });
+    // interagir
+    userEvent.click(homeEl);
+    // testar
+    expect(pathname).toBe('/');
+  });
+
+  it('Testa se o app é redirecionado para a URL /about ao clicar no link About', () => {
+    // acessar
+    const { history } = renderWithRouter(<App />);
+    const { pathname } = history.location;
+    const aboutEl = screen.getByRole('link', { name: /About/i });
+    // interagir
+    userEvent.click(aboutEl);
+    // testar
+    expect(pathname).toBe('/about');
+  });
+  it('Ao clicar em Favorites Pokémons o app é redirecionado para URL /favorites ', () => {
+    // acessar
+    const { history } = renderWithRouter(<App />);
+    const { pathname } = history.location;
+    const favoritePokemonEl = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    // interagir
+    userEvent.click(favoritePokemonEl);
+    // testar
+    expect(pathname).toBe('/favorites');
+  });
+  it('Ao entrar em uma URL desconhecida o app é redirecionado para pág Not Found', () => {
+    // acessar
+    const { history } = renderWithRouter(<App />);
+    history.push('/paginaNaoEncontrada');
+    const notFoundText = screen.getByRole(
+      'heading',
+      { name: /Page requested not found/i },
+      { level: 2 },
+    );
+    // testar
+    expect(notFoundText).toBeInTheDocument();
+  });
+});
