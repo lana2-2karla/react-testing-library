6a70062cf745b0680ff0af68dd883ececfb1e89f
Allana Karla <allanakarla1702@gmail.com>
2022-02-23 11:57:11 -0300

    Requisito 5 incompleto

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,5 +1,26 @@
-describe('', () => {
-  it('', () => {
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../components/RenderWithRouter';
+import App from '../App';
 
+const { screen } = require('@testing-library/react');
+
+describe('Testa o componente <Pokedex.js />', () => {
+  it('Testa se a página contém título "Encountered pokémons"', () => {
+    // acessar
+    renderWithRouter(<App />);
+    const titleEncounteredPokemons = screen
+      .getByRole('heading', { name: /Encountered Pokémons/i, level: 2 });
+    // testar
+    expect(titleEncounteredPokemons).toBeInTheDocument();
+  });
+  it('Testa se é exibido o próximo Pokémon da lista ao clicar no botão ', () => {
+    // Testa se botão está na tela
+    renderWithRouter(<App />);
+    const btnNextPokemon = screen
+      .getByRole('button', { name: /Próximo Pokémon/i });
+    expect(btnNextPokemon).toBeInTheDocument();
+    // Testa se todos os pokémons são listados
+    const btnsPokémons = screen.getAllByTestId('pokemon-type-button');
   });
 });
